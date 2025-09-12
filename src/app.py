4ca7ea8f38cd92fa02ccb5750338b84e6b6492c8
ctrl ê <eretyho@gmail.com>
2025-09-12 18:27:53 -0300

    add

--- src/app.py
+++ src/app.py
@@ -1,10 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
-df_0 = pd.ExcelFile('./data/raw.xlsx').sheet_names
-print(len(df_0))
+def create_raw_csv_files():
+    for i in range(0, 10):
+        df = pd.read_excel('./data/raw.xlsx', sheet_name=i)
+        df.to_csv(f'./data/raw_{i}.csv', index=False)
 
-for i in range(len(df_0)):
-    df = pd.read_excel('./data/raw.xlsx', sheet_name=i)
-    df.to_csv(f'./data/raw_{i}.csv', index=False)
+df_0 = pd.read_csv('./data/raw_0.csv')
+
+consumo = df_0['Consumo']
+print(consumo)
+consumo_media = consumo.mean()
+print(consumo_media)
+consumo_mediana = consumo.median()
+print(consumo_mediana)
+consumo_moda = consumo.mode()
+print(consumo_moda)
+
+fig, ax = plt.subplots(figsize=(6,8))
+
+ax.hist(consumo, bins=12)
+ax.set_xlabel('Consumo MW/h')
+ax.set_ylabel('Frequencia')
+plt.show()

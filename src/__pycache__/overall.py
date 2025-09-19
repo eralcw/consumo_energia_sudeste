ad56f8748ec88e4d8485d14fd70e2c0d27d30348
ctrl ê <eretyho@gmail.com>
2025-09-19 20:49:45 -0300

    Add

--- src/__pycache__/overall.py
+++ src/__pycache__/overall.py
@@ -76,5 +76,27 @@ def overall_region_class_average():
         print(f'Erro ao calcular overall por Regiao e Classe: {e}')
         return False
 
+def overall_col_col_average(coluna_1, coluna_2):
+    try:
+        df = pd.read_csv('./data/raw/raw_0.csv')
+        show = (
+            df.groupby([coluna_1, coluna_2])['Consumo']
+            .agg(
+                Quantidade='count',
+                Media='mean',
+                Mediana='median',
+                Moda=lambda x : x.mode()[0] if not x.mode().empty else None,
+                Maior_Consumo='max',
+                Menor_Consumo='min',
+            )
+            .reset_index()
+        )
+        print(show)
+        path = f'./data/processed/overall_{coluna_1.lower()}_{coluna_2.lower()}_average.csv'
+        if not os.path.exists(path):
+            show.to_csv(path, index=False)
+    except Exception as e:
+        print(f'Erro ao calcular overall por Regiao e Classe: {e}')
+        return False
 if __name__=='__main__':
-    overall_region_class_average()
+    overall_col_col_average('Sistema', 'Classe')
